cfa8f1babd984621e66deb936a9243618ca461b0
darrel2411 <tapsdarrel@gmail.com>
2021-07-02 01:35:49 +0800

    new Development

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended: true}))
+app.use(express.static(__dirname + '/public'));
 
 app.use(session({
     secret: 'keyboard cat',
--- middlewares/isLoginMiddleware.js
+++ middlewares/isLoginMiddleware.js
@@ -3,6 +3,7 @@
 const userIsLogin = (req,res,next) => {
     console.log(req.session.isLogin)
     if(req.session.isLogin){
+        document.getElementById("register").classList.add('invisible');
         next()
     } else {
         res.redirect('/clothes/login')
